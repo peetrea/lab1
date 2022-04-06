34484264f4db6ce20845734f12e6e32f8f123ee7
peetrea <boss.tankionline01@gmail.com>
2022-04-06 16:24:43 +0300

    add lab4

--- lab2/src/App.tsx
+++ lab2/src/App.tsx
@@ -1,27 +1,49 @@
+
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import 'antd/dist/antd.css';
+import Layout_content from "./components/Layout"
+import {StudenOverview} from "./components/Overview"
 
 
-import { LayoutCos } from './Layouts/LayoutCos'
+const infoStudent = [
+  {
+    firstName: "Petru",
+    secondName:"Cristea",
+    gender:"male",
+    age:21,
+    height:200,
+    univerity: "UTM",
+    speciality:"RM",
+  },
+  {
+    firstName: "Rusnac",
+    secondName:"Sandu",
+    gender:"male",
+    age:13,
+    height:30,
+    univerity: "Hogwarts",
+    speciality:"Slytherin",
+
+  },
+  {
+    firstName: "Jimbei",
+    secondName:"Alexandru",
+    gender:"male",
+    age:22,
+    height:180,
+    univerity: "UTM",
+    speciality:"RM",
+  }
+]
 
 function App() {
-  const [input1, setInput1] = React.useState("")
-  const [input2, setInput2] = React.useState("")
-  const [input3, setInput3] = React.useState("")
-  
-
   return (
-    <div>
-      <LayoutCos />
-      <input value={input1} onChange={(e) => {setInput1(e.target.value)}} placeholder="Input 1" type="text" id="fname"></input>
-      <input value={input2} onChange={(e) => {setInput2(e.target.value)}} placeholder="Input 2"></input>
-      <input value={input3} onChange={(e) => {setInput3(e.target.value)}} placeholder="Input 3"></input>
-      <p> {input1} </p>
-      <button onClick={()=>alert(input1)}>Button 1</button>
-      <button onClick={()=>alert(input2)}>Button 2</button>
-      <button onClick={()=>alert(input3)}>Button 3</button>
+    <div className="App">
+        <Layout_content
+        />
+        <StudenOverview 
+        students = {infoStudent}
+        />
     </div>
   );
 }
